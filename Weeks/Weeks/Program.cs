2ba3c00057d00b6bd3c489a3576b10088aee99ab
unknown <poojaaurwade@gmail.com>
2023-01-14 20:35:07 +0530

    pooja

---
+++ Weeks/Weeks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class WeekDays
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number between 1 and 7:");
+        int input = int.Parse(Console.ReadLine());
+
+        switch (input)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 7.");
+                break;
+        }
+    }
+}
